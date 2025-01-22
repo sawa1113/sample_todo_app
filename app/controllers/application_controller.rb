9e51731c651d77d8055a7c91da4c5a89919ef551
sawa1113 <63143529+sawa1113@users.noreply.github.com>
2025-01-22 09:26:25 +0900

    ユーザー認証機能の実装(deviseの導入)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
     include Pagy::Backend
+
+    # ログアウト後のリダイレクト先を設定
+    def after_sign_out_path_for(resource_or_scope)
+        new_user_session_path
+    end
 end
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,22 +1,24 @@
 class TodosController < ApplicationController
+  before_action :authenticate_user! # ログインしていない場合アクセス不可
+  before_action :set_todo, only: %i[show edit update destroy]
+  before_action :authorize_user, only: %i[edit update destroy remove_attachment]
+
   def index
-    @pagy, @todos = pagy(Todo.all, items: 10)
+    @pagy, @todos = pagy(current_user.todos, items: 10) # ログインユーザーのタスクのみ
   end
 
   def show
-    @todo = Todo.find(params[:id])
   end
 
   def new
-    @todo = Todo.new
+    @todo = current_user.todos.build # ログインユーザーのタスクとして初期化
   end
 
   def edit
-    @todo = Todo.find(params[:id])
   end
 
   def create
-    @todo = Todo.new(todo_params)
+    @todo = current_user.todos.build(todo_params) # ログインユーザーに関連付け
 
     if @todo.save
         redirect_to @todo, notice: 'Todoを作成しました。'
@@ -26,8 +28,6 @@ class TodosController < ApplicationController
   end
 
   def update
-    @todo = Todo.find(params[:id])
-  
     if @todo.update(todo_params)
       redirect_to @todo, notice: 'Todoを更新しました。'
     else
@@ -39,16 +39,12 @@ class TodosController < ApplicationController
   end
 
   def destroy
-    @todo = Todo.find(params[:id])
     @todo.destroy
-
     redirect_to todos_path, notice: 'Todoを削除しました。'
   end
 
   #画像削除アクション
   def remove_attachment
-    @todo = Todo.find(params[:id])
-
     if @todo.remove_attachment
       redirect_to @todo, notice: "添付ファイルが削除されました。"
     else
@@ -57,6 +53,14 @@ class TodosController < ApplicationController
   end
 
   private
+
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
+
+  def authorize_user
+    redirect_to todos_path, alert: '権限がありません。' unless @todo.user == current_user
+  end
 
   def todo_params
     params.require(:todo).permit(:title, :description, :completed, :attachment)
